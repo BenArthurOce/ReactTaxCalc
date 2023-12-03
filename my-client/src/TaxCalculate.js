cdf5f871bf48d78854d69ab7047d19d99c3a15ea
Ben Arthur <benarthur@live.com>
2023-12-03 15:22:26 +1100

    api successfully working

--- my-client/src/TaxCalculate.js
+++ my-client/src/TaxCalculate.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 
 function TaxCalculate(props) {
     const [incomeTax, setIncomeTax] = useState('');
@@ -8,77 +8,67 @@ function TaxCalculate(props) {
     const [lowMiddleIncomeOffset, setLowMiddleIncomeOffset] = useState('');
     const [hecsRepayment, setHecsRepayment] = useState('');
 
-    console.log(props)
-    const income = 70000
-    const year = 2023
 
+//     props.onTaxCalculation({
+//         incomeTax: incomeTax
+//        ,medicare: medicareLevy
+//        ,mls: medicareLevySurcharge
+//        ,lito: lowIncomeOffset
+//        ,lmito: lowMiddleIncomeOffset
+//        ,hecs: hecsRepayment
+//    });
+
+
+
+    console.log("TaxCalculate props")
+    console.log(props)
+
+
+    const calculateTax = () => {
+        // const { income, year, apiData } = props;
+        const Zincome = props.formData.income
+        const Zyear =  props.formData.year
+        const Zdata = props.apiData
+
+
+        if (props.apiData) {
+            // const brackets = props.apiData[Zyear]?.brackets || [];
+            const brackets = Zdata[Zyear]['brackets']
+            const result = getTaxPayable(Zincome, Zyear, brackets);
+            console.log(result)
+            setIncomeTax(result);
+
+            props.onTaxCalculation({
+                incomeTax: result,
+                // ... other values you want to pass
+            });
+        }
+    };
+
+    // useEffect(() => {
+    //     calculateTax();
+    // }, [props.formData]);
 
     useEffect(() => {
-    if (props.apiData) {
-        const brackets = props.apiData[year]?.brackets || [];
-        const result = getTaxPayable(income, year, props.apiData);
-        setIncomeTax(result);
-        }
-    }, [income, year, props.apiData]);
+        calculateTax();
+    }, []);
 
-
-    props.onTaxCalculation({
-        incomeTax: incomeTax
-       ,medicare: medicareLevy
-       ,mls: medicareLevySurcharge
-       ,lito: lowIncomeOffset
-       ,lmito: lowMiddleIncomeOffset
-       ,hecs: hecsRepayment
-   });
-
-
-    let taxPayable = 0
-
-    const getTaxPayable = (income, year, apiData) => {
-
-        console.log(income)
-        console.log(year)
-        console.log(apiData)
-        const { brackets } = apiData[year];
-
+    const getTaxPayable = (income, year, brackets) => {
+        console.log(`  income = ${income}    year = ${year}    brackets = ${brackets}`)
         for (const { range, rate, baseAmount } of brackets) {
-          if (income >= range[0] && income <= range[1]) {
-            const result = ((income - range[0]) * rate) + baseAmount;
-            return result;
-          }
+            if (income >= range[0] && income <= range[1]) {
+                const result = ((income - range[0]) * rate) + baseAmount;
+                return result;
+            }
         }
-    }
-
+        return 0;
+    };
 
     return (
         <div>
             <p>{incomeTax}</p>
         </div>
-    )
-};
+    );
+}
 
 export default TaxCalculate;
-
-
-
-// const TaxCalculate = ({ income, year, apiData }) => {
-//     const [taxPayable, setTaxPayable] = useState(0);
-
-//     useEffect(() => {
-//         if (apiData) {
-//             const brackets = apiData[year]?.brackets || [];
-//             const result = getTaxPayable(income, brackets);
-//             setTaxPayable(result);
-//         }
-//     }, [income, year, apiData]);
-
-//     const getTaxPayable = (income, brackets) => {
-//         // ... existing getTaxPayable logic ...
-//     };
-
-//     return (
-//         <div>
-//             {/* Display or use taxPayable as needed */}
-//         </div>
-//     );
-// };
--- my-client/src/TaxRequest.js
+++ my-client/src/TaxRequest.js
@@ -11,6 +11,9 @@ function TaxRequest(props) {
 
   const [apiData2, setData2] = useState(null);
 
+  console.log("TaxRequest props")
+  console.log(props)
+
   useEffect(() => {
     const loadTaxData = async () => {
       setErrorFlag(null);
@@ -28,7 +31,7 @@ function TaxRequest(props) {
     };
 
     loadTaxData();
-  }, [props.formData]);
+  }, []);
 
   return (
     
@@ -40,7 +43,11 @@ function TaxRequest(props) {
       {loadingFlag && <p>Loading Results</p>}
       {errorFlag && <p>Error loading results: {errorFlag.message}</p>}
       {apiData && (
-        <TaxCalculate apiData={apiData} onTaxCalculation={setData2}/>
+        <TaxCalculate 
+            apiData={apiData} 
+            onTaxCalculation={setData}
+            formData={props.formData}
+        />
       )}
     </div>
   );
@@ -49,3 +56,63 @@ function TaxRequest(props) {
 export default TaxRequest;
 
 {/* <TaxRequest formData={formData} onAPIRequest={performResultState} /> */}
+
+
+// {formData && (
+//     <TaxRequest
+//     formData={formData}
+//     onAPIRequest={performResultState}
+//     onError={performErrorState}
+//     />
+// )}
+
+// import React, { useState, useEffect } from 'react';
+// import axios from 'axios';
+// import TaxCalculate from './TaxCalculate';
+
+// function TaxRequest(props) {
+//   const API_URL = '/IncomeTax'; // Express server backend url
+
+//   const [apiData, setApiData] = useState(null);
+//   const [loadingFlag, setLoadingFlag] = useState(false);
+//   const [errorFlag, setErrorFlag] = useState(false);
+
+//   useEffect(() => {
+//     const loadTaxData = async () => {
+//       setErrorFlag(null);
+//       setLoadingFlag(true);
+
+//       try {
+//         const res = await axios.get(API_URL, { params: { formData: props.formData } });
+//         setApiData(res.data);
+//         setErrorFlag(false);
+//       } catch (err) {
+//         setErrorFlag(true);
+//       } finally {
+//         setLoadingFlag(false);
+//       }
+//     };
+
+//     loadTaxData();
+//   }, [props.formData]);
+
+//   return (
+//     <div>
+//       <h1>COMPONENT: TaxCalculation</h1>
+//       <h2>Calculation Results</h2>
+
+//       {/* conditional rendering */}
+//       {loadingFlag && <p>Loading Results</p>}
+//       {errorFlag && <p>Error loading results: {errorFlag.message}</p>}
+//       {apiData && (
+//         <TaxCalculate
+//           apiData={apiData}
+//           formData={props.formData}  // Pass the form data to TaxCalculate
+//           onTaxCalculation={props.onAPIRequest}  // Pass the callback function to TaxCalculate
+//         />
+//       )}
+//     </div>
+//   );
+// }
+
+// export default TaxRequest;
